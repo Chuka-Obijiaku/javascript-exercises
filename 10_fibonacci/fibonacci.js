afcaf21bdedee97dad02b3614339784ead2f1957
chuka <22582292+Chuka-Obijiaku@users.noreply.github.com>
2023-11-06 13:34:47 -0600

    10 Fibonacci Complete

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,4 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(fibIndex) {
+    if (fibIndex < 0) {
+        return 'OOPS'
+    }
+    if (fibIndex == 0) {
+        return 0;
+    }
+    if (fibIndex == 1) {
+        return 1;
+    }
+    let num_2 = 0;
+    let num_1 = 1;
+    for (let i = 2; i <= fibIndex; i++) {
+        let currentNum = num_2 + num_1;
+        num_2 = num_1;
+        num_1 = currentNum;
+    }
+    return num_1;
 
 };
 
